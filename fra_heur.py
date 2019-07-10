859189c670d35133dd778e6a21998c702c883b90
Stefan Schwarze <unegj@student.kit.edu>
2019-07-10 17:05:06 +0200

    clean up

--- fra_heur.py
+++ fra_heur.py
@@ -1,14 +1,12 @@
 
 import gc, weakref, pytest
-from pyscipopt import Model, Heur, SCIP_RESULT, SCIP_PARAMSETTING, SCIP_HEURTIMING, quicksum
-from pyscipopt.scip import is_memory_freed
-#from util import is_optimized_mode
+from pyscipopt import Model, Heur, SCIP_RESULT, SCIP_HEURTIMING, quicksum
 
 #
-# heuristic
+# heuristic: feasibe rounding approach
 #
 
-class RoundingHeur(Heur):
+class feasiblerounding(Heur):
 
     def heurinitsol(self):
         print(">>>> call heurinitsol()")
@@ -19,41 +17,42 @@ class RoundingHeur(Heur):
     # execution method of the heuristic
     def heurexec(self, heurtiming, nodeinfeasible):
 
-        print(">>>> Call feasible rounding heuristisc at a node with depth %d" % (self.model.getDepth()))
+        print(">>>> Call feasible rounding heuristic at a node with depth %d" % (self.model.getDepth()))
 
         # TODO:(Wichtig, damit IPS größer wird) Fixierte Variablen müssen aus dem LP rausgenommen werden / oder ein neues LP ohne die Variablen erstellen
 
         # Modell für innere Parallelmenge erstellen
-        ips_model = Model()
+        print(">>>> Build inner parallel set")
+        ips_model = Model("ips")
+        # Variablen zum Modell hinzufügen
         variables = self.model.getVars()
         for v in variables:
             ips_model.addVar(name=v.name,
                              vtype=v.vtype(),
-                             lb = v.getLbLocal(),
-                             ub = v.getUbLocal(),
-                             obj = v.getObj())
+                             lb=v.getLbLocal(),
+                             ub=v.getUbLocal(),
+                             obj=v.getObj())
 
-        # TODO: Objective hinzufügen
+        # Zielfunktion
+        ips_model.setObjective(self.model.getObjective())
 
-        linear_rows_data = self.model.getLPRowsData()  # linear rows of current LP-solution in one List: Methods from 'Row'-class can be applied to them
-        print('>>> Execution of Rows')
-        # iterate through all rows and update the RHS
-        for lrow in linear_rows_data:
+        # Durch Rows iterieren und modifizierte Ungleichungen hinzufügen
+        linear_rows = self.model.getLPRowsData()
+        for lrow in linear_rows:
             vlist = [i.getVar() for i in lrow.getCols()]
             clist = lrow.getVals()
             beta = 0
             for i in range(len(clist)):
                 if (vlist[i].vtype()!='CONTINUOUS'):
                     beta += abs(clist[i])
-            ips_model.addCons(quicksum([vlist[i]*clist[i] for i in range(len(vlist))]) <= lrow.getRhs()-0.5*beta)
+
+            ips_model.addCons(quicksum(vlist[i]*clist[i] for i in range(len(vlist))) <= lrow.getRhs()-0.5*beta)
 
         ips_model.optimize()
+        print(">>>> Optimized over inner parallel set")
 
-        print('>>> Done with Rows')
-        # create solution
+        # Platzhalter: create solution
         sol = self.model.createSol()
-        #for v in vars:
-        #   self.model.setSolVal(sol, v, 0.0)
         accepted = self.model.trySol(sol)
         print(">>>> accepted solution? %s" % ("yes" if accepted == 1 else "no"))
 
@@ -69,7 +68,7 @@ def test_heur():
     m = Model()
 
     # create and add heuristic to SCIP
-    heuristic = RoundingHeur()
+    heuristic = feasiblerounding()
     m.includeHeur(heuristic, "PyHeur", "feasible rounding heuristic", "Y", timingmask=SCIP_HEURTIMING.AFTERLPNODE, freq=0)
 
     # read problem from file
