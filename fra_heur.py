232ca6c88ee7fc525c0234ba0a7a14b8eaf16efd
Stefan Schwarze <unegj@student.kit.edu>
2019-08-01 10:30:11 +0200

    Umsetzung von: -createSol(): aus Lösung des Submodells primale Lösung erstellen -test_heur auf mehreren Instanzen

--- fra_heur.py
+++ fra_heur.py
@@ -1,7 +1,8 @@
 
-import gc, weakref, pytest
+import os, glob
 from pyscipopt import Model, Heur, SCIP_RESULT, SCIP_HEURTIMING, quicksum
 import math
+
 
 #
 # heuristic: feasibe rounding approach
@@ -17,7 +18,7 @@ class feasiblerounding(Heur):
 
     # execution method of the heuristic
     def heurexec(self, heurtiming, nodeinfeasible):
-        delta = 0.99
+        delta = 0.999
         gran = True
 
         print(">>>> Call feasible rounding heuristic at a node with depth %d" % (self.model.getDepth()))
@@ -32,14 +33,15 @@ class feasiblerounding(Heur):
         variables = self.model.getVars(transformed=True)
         ips_vars = dict()
         for v in variables:
-            if v.vtype!='CONTINUOUS':
+            if v.vtype != 'CONTINUOUS':
                 lb = math.ceil(v.getLbLocal()) - delta + 0.5
                 ub = math.floor(v.getUbLocal()) + delta - 0.5
-                ips_vars['v.name'] = ips_model.addVar(name=v.name , vtype='CONTINUOUS', lb=lb, ub=ub, obj=v.getObj())
+                ips_vars[v.name] = ips_model.addVar(name=v.name, vtype='CONTINUOUS', lb=lb, ub=ub, obj=v.getObj())
             else:
-                ips_vars['v.name'] = ips_model.addVar(name=v.name , vtype='CONTINUOUS', lb=v.getLbLocal(), ub=v.getUbLocal(), obj=v.getObj())
+                ips_vars[v.name] = ips_model.addVar(name=v.name, vtype='CONTINUOUS', lb=v.getLbLocal(),
+                                                      ub=v.getUbLocal(), obj=v.getObj())
 
-        # Zielfunktion
+        # Zielfunktion setzen !
         # ips_model.setObjective(self.model.getObjective())
 
         # Durch Rows iterieren und modifizierte Ungleichungen hinzufügen
@@ -51,28 +53,28 @@ class feasiblerounding(Heur):
             beta = sum(abs(clist[i]) for i in range(len(clist)) if vlist[i].vtype != 'CONTINUOUS')
             # enlarged inner parallel set
             current_delta = 0
-            if all(vlist[i].vtype != 'CONTINUOUS' for i in range(len(clist))) and all(clist[i].is_integer() for i in range(len(clist))):
+            if all(vlist[i].vtype != 'CONTINUOUS' for i in range(len(clist))) and all(
+                    clist[i].is_integer() for i in range(len(clist))):
                 current_delta = delta
-                print('enlargement possible!')
-            print(lrow.getLhs())
-            print(lrow.getRhs())
-            lhs = lrow.getLhs() + 0.5*beta - current_delta
-            rhs = lrow.getRhs() - 0.5*beta + current_delta
 
-            if lhs > (rhs + 10**(-6)):
+            lhs = lrow.getLhs() + 0.5 * beta - current_delta
+            rhs = lrow.getRhs() - 0.5 * beta + current_delta
+
+            if lhs > (rhs + 10 ** (-6)):
                 print('(Sub-)Problem is not granular')
                 gran = False
                 break
 
-            ips_model.addCons(lhs <= (quicksum(ips_vars[vlist[i].name]*clist[i] for i in range(len(vlist))) <= rhs))
+            ips_model.addCons(lhs <= (quicksum(ips_vars[vlist[i].name] * clist[i] for i in range(len(vlist))) <= rhs))
 
         if gran:
-
             print(">>>> Optimize over inner parallel set")
             ips_model.optimize()
-
-            # TODO: Rundung vom Optimum als Lösung übergeben
+            
             sol = self.model.createSol()
+            for v in variables:
+                self.model.setSolVal(sol,v,ips_model.getVal(ips_vars[v.name]))
+            
             accepted = self.model.trySol(sol)
             print(">>>> accepted solution? %s" % ("yes" if accepted == 1 else "no"))
 
@@ -84,23 +86,37 @@ class feasiblerounding(Heur):
             return {"result": SCIP_RESULT.DIDNOTFIND}
 
 
-def test_heur():
+def test_heur(testruns = 3):
+    path_to_problems = '/home/stefan/Dokumente/opt_problems/miplib2010_benchmark/'
+    os.chdir(path_to_problems)
+    problem_names = glob.glob("*.mps")
+    
+    iteration = 0
+    
+    for problem in problem_names:
+        
+        iteration += 1
+        # create model
+        m = Model()
+    
+        # create and add heuristic to SCIP
+        heuristic = feasiblerounding()
+        m.includeHeur(heuristic, "PyHeur", "feasible rounding heuristic", "Y", timingmask=SCIP_HEURTIMING.AFTERLPNODE,
+                      freq=0)
 
-    # create model
-    m = Model()
+        # read exemplary problem from file
+        print('>>>>> Working on Problem: ',problem)
+        m.readProblem("".join([path_to_problems,problem]))
+    
+        # optimize problem
+        m.optimize()
 
-    # create and add heuristic to SCIP
-    heuristic = feasiblerounding()
-    m.includeHeur(heuristic, "PyHeur", "feasible rounding heuristic", "Y", timingmask=SCIP_HEURTIMING.AFTERLPNODE, freq=0)
-
-    # read exemplary problem from file
-    m.readProblem('/home/stefan/Dokumente/02_HiWi_IOR/mps-files/50v-10.mps')
-
-    # optimize problem
-    m.optimize()
-
-    # free model explicitly
-    del m
+        # free model explicitly
+        del m
+        
+        if iteration >= testruns:
+            break
+        
 
 if __name__ == "__main__":
     test_heur()
