17e5402df35c44c5c6d8ce46526ba40416283db7
Stefan Schwarze <unegj@student.kit.edu>
2019-08-05 17:02:36 +0200

    ZF des Subproblems hinzugefügt mit linearer Expression  - Frage offen, ob die ursprüngliche ZF mit transformierten Variablen abgerufen werden kann

--- fra_heur.py
+++ fra_heur.py
@@ -20,7 +20,7 @@ class feasiblerounding(Heur):
     # execution method of the heuristic
     def heurexec(self, heurtiming, nodeinfeasible):
         delta = 0.999
-        gran = True
+        solvable_ips = True
 
         print(">>>> Call feasible rounding heuristic at a node with depth %d" % (self.model.getDepth()))
 
@@ -39,51 +39,25 @@ class feasiblerounding(Heur):
                 ub = math.floor(v.getUbLocal()) + delta - 0.5
                 ips_vars[v.name] = ips_model.addVar(name=v.name, vtype='CONTINUOUS', lb=lb, ub=ub, obj=v.getObj())
             else:
-                ips_vars[v.name] = ips_model.addVar(name=v.name, vtype='CONTINUOUS', lb=v.getLbLocal(),
-                                                      ub=v.getUbLocal(), obj=v.getObj())
+                ips_vars[v.name] = ips_model.addVar(name=v.name, vtype='CONTINUOUS', lb=v.getLbLocal(), ub=v.getUbLocal(), obj=v.getObj())
 
         # Zielfunktion setzen 
         zf_ex = self.model.getObjective()
         zf_sense = self.model.getObjectiveSense()
         zf_ex.normalize()
         zf_items = zf_ex.terms.items()
-        new_expr = quicksum(ips_vars[v[0].name]*coef for v,coef in zf_items if len(v)!=0)
+        # Nur für lineare Zielfunktionen, Konstanten werden rausgelassen
+        new_expr = quicksum(ips_vars['t_'+v[0].name]*coef for v,coef in zf_items if len(v)!=0)
+        ips_model.setObjective(new_expr,sense=zf_sense)
         
-        #ips_model.setObjective(quicksum(ips_vars[v.name]*coef for v , coef in zf_ex.terms.items()),sense=zf_sense)
-        
-        '''
-        if Expr._is_number(zf_ex):
-            zf = Constant(zf_ex)
-        elif isinstance(zf_ex, Expr):
-            # loop over terms and create a sumexpr with the sum of each term
-            # each term is either a variable (which gets transformed into varexpr)
-            # or a product of variables (which gets tranformed into a prod)
-            sumexpr = SumExpr()
-            for vars, coef in zf_ex.terms.items():
-                 if len(vars) == 0:
-                     sumexpr += coef
-                 elif len(vars) == 1:
-                     # add the 'new" Variables in the Goalfunction
-                     varexpr = VarExpr(ips_vars[vars[0].name])
-                     sumexpr += coef * varexpr
-                 # unused, because function is linear
-                 else:
-                     prodexpr = ProdExpr()
-                     for v in vars:
-                          varexpr = VarExpr(ips_vars[v.name])
-                          prodexpr *= varexpr
-                     sumexpr += coef * prodexpr
-            zf = sumexpr
-        '''
-
         # Durch Rows iterieren und modifizierte Ungleichungen hinzufügen
         linear_rows = self.model.getLPRowsData()
         for lrow in linear_rows:
             vlist = [i.getVar() for i in lrow.getCols()]
             clist = lrow.getVals()
-
             beta = sum(abs(clist[i]) for i in range(len(clist)) if vlist[i].vtype != 'CONTINUOUS')
-            # enlarged inner parallel set
+            
+            # Vergrößerte IPM
             current_delta = 0
             if all(vlist[i].vtype != 'CONTINUOUS' for i in range(len(clist))) and all(
                     clist[i].is_integer() for i in range(len(clist))):
@@ -91,15 +65,17 @@ class feasiblerounding(Heur):
 
             lhs = lrow.getLhs() + 0.5 * beta - current_delta
             rhs = lrow.getRhs() - 0.5 * beta + current_delta
-
+            
+            # Unlösbarkeit abfangen
             if lhs > (rhs + 10 ** (-6)):
                 print('>>>> EIPS is empty')
-                gran = False
+                solvable_ips = False
                 break
-
+            
+            # Ungleichung dem Modell hinzufügen
             ips_model.addCons(lhs <= (quicksum(ips_vars[vlist[i].name] * clist[i] for i in range(len(vlist))) <= rhs))
 
-        if gran:
+        if solvable_ips:
             print(">>>> Optimize over EIPS")
             ips_model.optimize()
             
@@ -109,7 +85,7 @@ class feasiblerounding(Heur):
                     self.model.setSolVal(sol,v,round(ips_model.getVal(ips_vars[v.name])))
                 else:
                     self.model.setSolVal(sol,v,ips_model.getVal(ips_vars[v.name]))
-            
+
             accepted = self.model.trySol(sol)
             print(">>>> accepted solution? %s" % ("yes" if accepted == 1 else "no"))
 
