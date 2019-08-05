b46a55f6244937e67764cc236b7a136f98220032
Stefan Schwarze <unegj@student.kit.edu>
2019-08-05 16:40:10 +0200

    ZF für Subproblem  hinzufügen Zwischenspeichern

--- fra_heur.py
+++ fra_heur.py
@@ -1,6 +1,7 @@
 
 import os, glob
-from pyscipopt import Model, Heur, SCIP_RESULT, SCIP_HEURTIMING, quicksum
+from pyscipopt import Model, Heur, SCIP_RESULT, SCIP_HEURTIMING, quicksum, Expr
+#from pyscipopt  import Constant, SumExpr, Expr, VarExpr, ProdExpr
 import math
 
 
@@ -26,7 +27,7 @@ class feasiblerounding(Heur):
         # TODO:(Wichtig, damit IPS größer wird) Fixierte Variablen müssen aus dem LP rausgenommen werden / oder ein neues LP ohne die Variablen erstellen
 
         # Modell für innere Parallelmenge erstellen
-        print(">>>> Build inner parallel set")
+        print(">>>> Build IPS")
         ips_model = Model("ips")
 
         # Variablen zum Modell hinzufügen + vergrößern der Box-Constraints
@@ -41,8 +42,39 @@ class feasiblerounding(Heur):
                 ips_vars[v.name] = ips_model.addVar(name=v.name, vtype='CONTINUOUS', lb=v.getLbLocal(),
                                                       ub=v.getUbLocal(), obj=v.getObj())
 
-        # Zielfunktion setzen !
-        # ips_model.setObjective(self.model.getObjective())
+        # Zielfunktion setzen 
+        zf_ex = self.model.getObjective()
+        zf_sense = self.model.getObjectiveSense()
+        zf_ex.normalize()
+        zf_items = zf_ex.terms.items()
+        new_expr = quicksum(ips_vars[v[0].name]*coef for v,coef in zf_items if len(v)!=0)
+        
+        #ips_model.setObjective(quicksum(ips_vars[v.name]*coef for v , coef in zf_ex.terms.items()),sense=zf_sense)
+        
+        '''
+        if Expr._is_number(zf_ex):
+            zf = Constant(zf_ex)
+        elif isinstance(zf_ex, Expr):
+            # loop over terms and create a sumexpr with the sum of each term
+            # each term is either a variable (which gets transformed into varexpr)
+            # or a product of variables (which gets tranformed into a prod)
+            sumexpr = SumExpr()
+            for vars, coef in zf_ex.terms.items():
+                 if len(vars) == 0:
+                     sumexpr += coef
+                 elif len(vars) == 1:
+                     # add the 'new" Variables in the Goalfunction
+                     varexpr = VarExpr(ips_vars[vars[0].name])
+                     sumexpr += coef * varexpr
+                 # unused, because function is linear
+                 else:
+                     prodexpr = ProdExpr()
+                     for v in vars:
+                          varexpr = VarExpr(ips_vars[v.name])
+                          prodexpr *= varexpr
+                     sumexpr += coef * prodexpr
+            zf = sumexpr
+        '''
 
         # Durch Rows iterieren und modifizierte Ungleichungen hinzufügen
         linear_rows = self.model.getLPRowsData()
@@ -61,19 +93,22 @@ class feasiblerounding(Heur):
             rhs = lrow.getRhs() - 0.5 * beta + current_delta
 
             if lhs > (rhs + 10 ** (-6)):
-                print('(Sub-)Problem is not granular')
+                print('>>>> EIPS is empty')
                 gran = False
                 break
 
             ips_model.addCons(lhs <= (quicksum(ips_vars[vlist[i].name] * clist[i] for i in range(len(vlist))) <= rhs))
 
         if gran:
-            print(">>>> Optimize over inner parallel set")
+            print(">>>> Optimize over EIPS")
             ips_model.optimize()
             
             sol = self.model.createSol()
             for v in variables:
-                self.model.setSolVal(sol,v,ips_model.getVal(ips_vars[v.name]))
+                if v.vtype != 'CONTINUOUS':
+                    self.model.setSolVal(sol,v,round(ips_model.getVal(ips_vars[v.name])))
+                else:
+                    self.model.setSolVal(sol,v,ips_model.getVal(ips_vars[v.name]))
             
             accepted = self.model.trySol(sol)
             print(">>>> accepted solution? %s" % ("yes" if accepted == 1 else "no"))
@@ -86,8 +121,8 @@ class feasiblerounding(Heur):
             return {"result": SCIP_RESULT.DIDNOTFIND}
 
 
-def test_heur(testruns = 3):
-    path_to_problems = '/home/stefan/Dokumente/opt_problems/miplib2010_benchmark/'
+def big_test_heur(testruns = 4):
+    path_to_problems = '/home/stefan/Dokumente/opt_problems/miplib2010_benchmark/small_subset/'
     os.chdir(path_to_problems)
     problem_names = glob.glob("*.mps")
     
@@ -116,7 +151,15 @@ def test_heur(testruns = 3):
         
         if iteration >= testruns:
             break
-        
+
+def test_single_heur():
+    m = Model()
+    heuristic = feasiblerounding()
+    m.includeHeur(heuristic, "PyHeur", "feasible rounding heuristic", "Y", timingmask=SCIP_HEURTIMING.AFTERLPNODE, freq=4)
+    m.readProblem('/home/stefan/Dokumente/opt_problems/miplib2017_benchmark/50v-10.mps')
+    m.optimize()
+    del m
+    
 
 if __name__ == "__main__":
-    test_heur()
+    test_single_heur()
