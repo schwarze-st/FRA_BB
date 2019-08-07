ec0a3aef1ec58841cd17b5417aebe7563b30889c
Stefan Schwarze <unegj@student.kit.edu>
2019-08-07 18:44:08 +0200

    TestScript für granulare Instanzen test_granularity() überprüft, ob granulare Instanzen nach der SCIP-Propagation im Root-Node noch granular sind

--- fra_heur.py
+++ fra_heur.py
@@ -1,12 +1,10 @@
-
 import os, glob
 from pyscipopt import Model, Heur, SCIP_RESULT, SCIP_HEURTIMING, quicksum, Expr
-#from pyscipopt  import Constant, SumExpr, Expr, VarExpr, ProdExpr
 import math
 
 
 #
-# heuristic: feasibe rounding approach
+# heuristic: feasible rounding approach
 #
 
 class feasiblerounding(Heur):
@@ -19,6 +17,8 @@ class feasiblerounding(Heur):
 
     # execution method of the heuristic
     def heurexec(self, heurtiming, nodeinfeasible):
+        global granular_problems, non_granular_problems
+
         delta = 0.999
         solvable_ips = True
 
@@ -39,103 +39,135 @@ class feasiblerounding(Heur):
                 ub = math.floor(v.getUbLocal()) + delta - 0.5
                 ips_vars[v.name] = ips_model.addVar(name=v.name, vtype='CONTINUOUS', lb=lb, ub=ub, obj=v.getObj())
             else:
-                ips_vars[v.name] = ips_model.addVar(name=v.name, vtype='CONTINUOUS', lb=v.getLbLocal(), ub=v.getUbLocal(), obj=v.getObj())
+                ips_vars[v.name] = ips_model.addVar(name=v.name, vtype='CONTINUOUS', lb=v.getLbLocal(),
+                                                    ub=v.getUbLocal(), obj=v.getObj())
 
         # Zielfunktion setzen 
-        zf_ex = self.model.getObjective()
+        obj_sub = Expr()
         zf_sense = self.model.getObjectiveSense()
-        zf_ex.normalize()
-        zf_items = zf_ex.terms.items()
+        k = 0
+        for v in variables:
+            coeff = v.getObj()
+            if coeff != 0:
+                obj_sub += coeff * ips_vars[v.name]
+                k = 1
+        assert k == 1, "Objective Function is empty"
+        obj_sub.normalize()
+        ips_model.setObjective(obj_sub, sense=zf_sense)
+
+        # zf_ex = self.model.getObjective()
+        # zf_ex.normalize()
+        # zf_items = zf_ex.terms.items()
         # Nur für lineare Zielfunktionen, Konstanten werden rausgelassen
-        new_expr = quicksum(ips_vars['t_'+v[0].name]*coef for v,coef in zf_items if len(v)!=0)
-        ips_model.setObjective(new_expr,sense=zf_sense)
-        
+        # new_expr = quicksum(ips_vars['t_'+v[0].name]*coef for v,coef in zf_items if len(v)!=0)
+        # ips_model.setObjective(new_expr,sense=zf_sense)
+
         # Durch Rows iterieren und modifizierte Ungleichungen hinzufügen
         linear_rows = self.model.getLPRowsData()
         for lrow in linear_rows:
             vlist = [i.getVar() for i in lrow.getCols()]
             clist = lrow.getVals()
             beta = sum(abs(clist[i]) for i in range(len(clist)) if vlist[i].vtype != 'CONTINUOUS')
-            
+
+            #print(vlist, clist)
             # Vergrößerte IPM
             current_delta = 0
             if all(vlist[i].vtype != 'CONTINUOUS' for i in range(len(clist))) and all(
                     clist[i].is_integer() for i in range(len(clist))):
                 current_delta = delta
 
+            #print(lrow.getLhs(), lrow.getRhs())
             lhs = lrow.getLhs() + 0.5 * beta - current_delta
             rhs = lrow.getRhs() - 0.5 * beta + current_delta
-            
+            #print(lhs, rhs)
+
             # Unlösbarkeit abfangen
             if lhs > (rhs + 10 ** (-6)):
                 print('>>>> EIPS is empty')
                 solvable_ips = False
                 break
-            
+
             # Ungleichung dem Modell hinzufügen
             ips_model.addCons(lhs <= (quicksum(ips_vars[vlist[i].name] * clist[i] for i in range(len(vlist))) <= rhs))
 
         if solvable_ips:
             print(">>>> Optimize over EIPS")
             ips_model.optimize()
-            
+
             sol = self.model.createSol()
             for v in variables:
                 if v.vtype != 'CONTINUOUS':
-                    self.model.setSolVal(sol,v,round(ips_model.getVal(ips_vars[v.name])))
+                    self.model.setSolVal(sol, v, round(ips_model.getVal(ips_vars[v.name])))
                 else:
-                    self.model.setSolVal(sol,v,ips_model.getVal(ips_vars[v.name]))
+                    self.model.setSolVal(sol, v, ips_model.getVal(ips_vars[v.name]))
 
             accepted = self.model.trySol(sol)
             print(">>>> accepted solution? %s" % ("yes" if accepted == 1 else "no"))
 
             if accepted:
+                granular_problems += 1
                 return {"result": SCIP_RESULT.FOUNDSOL}
             else:
+                non_granular_problems += 1
                 return {"result": SCIP_RESULT.DIDNOTFIND}
         else:
+            non_granular_problems += 1
             return {"result": SCIP_RESULT.DIDNOTFIND}
 
 
-def big_test_heur(testruns = 4):
-    path_to_problems = '/home/stefan/Dokumente/opt_problems/miplib2010_benchmark/small_subset/'
+granular_problems = 0
+non_granular_problems = 0
+
+
+def test_granularity():
+    path_to_problems = '/home/stefan/Dokumente/02_HiWi_IOR/Paper_BA/franumeric/selectedTestbed/'
     os.chdir(path_to_problems)
     problem_names = glob.glob("*.mps")
-    
+
+    global granular_problems
+    global non_granular_problems
+    number_of_instances = len(problem_names)
     iteration = 0
-    
+
     for problem in problem_names:
-        
         iteration += 1
         # create model
         m = Model()
-    
+
         # create and add heuristic to SCIP
         heuristic = feasiblerounding()
         m.includeHeur(heuristic, "PyHeur", "feasible rounding heuristic", "Y", timingmask=SCIP_HEURTIMING.AFTERLPNODE,
                       freq=0)
 
+        m.setParam("limits/time", 60)
+
         # read exemplary problem from file
-        print('>>>>> Working on Problem: ',problem)
-        m.readProblem("".join([path_to_problems,problem]))
-    
+        print('>>>>> Working on Problem: ', problem, ', which is number ', iteration, 'of ', number_of_instances)
+        m.readProblem("".join([path_to_problems, problem]))
+
         # optimize problem
         m.optimize()
 
         # free model explicitly
         del m
-        
-        if iteration >= testruns:
-            break
 
-def test_single_heur():
+    print(">>>>> Auswertung")
+    print("Instances in Testbed: ", number_of_instances)
+    print("granualar instances: ", granular_problems)
+    print("Non granular Problems: ", non_granular_problems)
+    print("Non tested in Timelimit: ", number_of_instances-granular_problems-non_granular_problems)
+
+
+def test_heur():
     m = Model()
     heuristic = feasiblerounding()
-    m.includeHeur(heuristic, "PyHeur", "feasible rounding heuristic", "Y", timingmask=SCIP_HEURTIMING.AFTERLPNODE, freq=4)
-    m.readProblem('/home/stefan/Dokumente/opt_problems/miplib2017_benchmark/50v-10.mps')
+    m.includeHeur(heuristic, "PyHeur", "feasible rounding heuristic", "Y", timingmask=SCIP_HEURTIMING.AFTERLPNODE,
+                  freq=15)
+    m.readProblem('/home/stefan/Dokumente/opt_problems/miplib2010_benchmark/dfn-gwin-UUM.mps')
     m.optimize()
     del m
-    
+
 
 if __name__ == "__main__":
-    test_single_heur()
+    #test_heur()
+    test_granularity()
