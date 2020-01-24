b4b85ce7d7a8177fc50b8fec42ff644ccc89f115
Christoph Neumann <christoph.neumann@kit.edu>
2020-01-24 10:16:00 +0100

    fixed error in results_list

--- root_node_analysis.py
+++ root_node_analysis.py
@@ -14,24 +14,29 @@ testbed = read_all_test_instances(folder_name)
 print('Testing %i problems'%len(testbed))
 results_list = []
 
-for model_name in testbed:
+for idx, model_name in enumerate(testbed):
     logging.info('testing problem ' + model_name)
     print('Testing model %s'%model_name)
     path_name = folder_name+'/'+ model_name
-    m = test_heur(path_name)
-    m.writeStatistics("statistics")
-    with open('statistics') as f:
-        content = f.readlines()
-    for line in content:
-        if 'PyHeur' in line:
-            line_of_interest = line.split()
-            line_of_interest.remove(':')
-            results_list.append(line_of_interest)
-    del m
-    print(results_list)
-    os.remove('statistics')
+    try:
+        m = test_heur(path_name)
+        m.writeStatistics("statistics")
+        with open('statistics') as f:
+            content = f.readlines()
+        for line in content:
+            if 'PyHeur' in line:
+                line_of_interest = line.split()
+                line_of_interest.remove(':')
+                results_list.append(line_of_interest)
+        del m
+        print(results_list)
+        os.remove('statistics')
+    except:
+        print('unexpected error occurred')
+        results_list.append([float('inf')]*6)
+    results_frame = pd.DataFrame(results_list, columns = ['Heuristic','ExecTime','SetupTime','Calls','Found','Best'])
+    results_frame.index = testbed[:idx+1]
+    results_frame.to_pickle('results/'+folder_name+'results')
+    print(results_frame)
 
-results_frame = pd.DataFrame(results_list, columns = ['Heuristic','ExecTime','SetupTime','Calls','Found','Best'])
-results_frame.index = testbed
-results_frame.to_pickle('results/'+folder_name+'results')
-print(results_frame)
+
