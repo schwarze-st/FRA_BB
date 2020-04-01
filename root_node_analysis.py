04e146170c8fb191ec0461ef1cdd7d3a468ee508
Christoph <christoph.neumann@kit.edu>
2020-04-01 17:30:52 +0200

    add a list of successful problems to ensure that the length matches in the end

--- root_node_analysis.py
+++ root_node_analysis.py
@@ -14,6 +14,8 @@ logging.info('>>>>>>>>>>>>>>>> Running testbed ' + folder_name + " "+ dt_string 
 testbed = read_all_test_instances(folder_name)
 print('Testing %i problems'%len(testbed))
 results_list = []
+problem_names = []
+error_probs = []
 
 for idx, model_name in enumerate(testbed):
     logging.info('testing problem ' + model_name)
@@ -32,14 +34,17 @@ for idx, model_name in enumerate(testbed):
         del m
         print(results_list)
         os.remove('statistics')
+        problem_names.append(model_name[:-4])
     except:
         print('unexpected error occurred')
         results_list.append([float('inf')]*6)
+        error_probs.append(model_name[:-4])
     results_frame = pd.DataFrame(results_list, columns = ['Heuristic','ExecTime','SetupTime','Calls','Found','Best'])
     results_frame.index = testbed[:idx+1]
     results_frame.to_pickle('results/'+folder_name+'results')
     print(results_frame)
 
 os.rename('temp_results.pickle','results/FRA_Scip.pickle')
-convert_dict_to_dataframe(testbed)
+convert_dict_to_dataframe(problem_names)
+print(error_probs)
 
